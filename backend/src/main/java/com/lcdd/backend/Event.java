04aa92f8c17952fc9cecd441b17a23c1c53ab3cf
Daniel <Danielmg_21@hotmail.es>
2020-02-11 11:38:13 +0100

    Inserted event-form in backend

---
+++ backend/src/main/java/com/lcdd/backend/Event.java
@@ -0,0 +1,81 @@
+package com.lcdd.backend;
+
+import java.awt.Image;
+
+import org.springframework.stereotype.Controller;
+
+
+public class Event {
+	
+	private String name;
+	private String game;
+	private String date;
+	private String location;
+	private String description;
+	private Image image;
+	
+	
+	public Event() {}
+	
+	public Event(
+			String name,
+			String game,
+			String date,
+			String location,
+			String description,
+			Image image) {
+		this.name = name;
+		this.game = game;
+		this.date = date;
+		this.location = location;
+		this.description = description;
+		this.image = image;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public void setGame(String game) {
+		this.game = game;
+	}
+	public String getGame() {
+		return this.game;
+	}
+	
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDate() {
+		return this.date;
+	}
+	
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getLocation() {
+		return this.location;
+	}
+	
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	
+	
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	public Image getImage() {
+		return this.image;
+	}
+}
---
+++ backend/src/main/java/com/lcdd/backend/EventFormController.java
@@ -0,0 +1,18 @@
+package com.lcdd.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventFormController {
+  
+  @RequestMapping("/event-form")
+  public String eventTemplate(Model model){
+    
+    int a = 1;
+	  model.addAttribute("event", a);
+    
+    return "event-form";
+  }
+}
