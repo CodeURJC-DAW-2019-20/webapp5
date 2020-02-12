23f5fcaff1caa2603e853d836f0ac547981bae6d
Miguel Jimenez <m.jimenez@pitagorinesgroup.com>
2020-02-12 18:38:54 +0100

    Added merch type for optimizing future charts

--- backend/src/main/java/com/lcdd/backend/pojo/Merchandising.java
+++ backend/src/main/java/com/lcdd/backend/pojo/Merchandising.java
@@ -3,63 +3,76 @@ package com.lcdd.backend.pojo;
 public class Merchandising {
 	private long id;
 	private String name;
-	private String type;
+	private long typeId;
 	private float price;
 	private float discount;
 	private int stock;
 	private String description;
 	
-	public Merchandising(long id, String name, String type, float price, float discount, int stock,
+	public Merchandising(long id, String name, long typeId, float price, float discount, int stock,
 			String description) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.type = type;
+		this.typeId = typeId;
 		this.price = price;
 		this.discount = discount;
 		this.stock = stock;
 		this.description = description;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getType() {
-		return type;
+
+	public long getTypeId() {
+		return typeId;
 	}
-	public void setType(String type) {
-		this.type = type;
+
+	public void setTypeId(long typeId) {
+		this.typeId = typeId;
 	}
+
 	public float getPrice() {
 		return price;
 	}
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
+
 	public float getDiscount() {
 		return discount;
 	}
+
 	public void setDiscount(float discount) {
 		this.discount = discount;
 	}
+
 	public int getStock() {
 		return stock;
 	}
+
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
