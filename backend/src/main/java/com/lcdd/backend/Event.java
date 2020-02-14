0312eb37c5a893b18046e0ccc63a8fa9f39c8abf
Oscar Rivas <oscar.rivas.99@gmail.com>
2020-02-14 18:13:36 +0100

    updates

--- backend/src/main/java/com/lcdd/backend/Event.java
+++ backend/src/main/java/com/lcdd/backend/Event.java
@@ -1,78 +1,26 @@
 package com.lcdd.backend;
 
+import java.util.Date;
 
 public class Event {
 	
+	private int id;
 	private String name;
-	private String game;
-	private String date;
-	private String hour;
-	private String award;
+	private int game_id;
+	private String place;
+	private Date date;
+	private String time; //tipo de dato TIME?
 	private String description;
+	private boolean is_tournament;
+	private boolean is_single_participant;
+	private float inscription_fee;
+	private int max_participants;
+	
+	//private String award;   ??
 	//private Image image;
 	
 	
 	public Event() {}
-	
-	public Event(
-			String name,
-			String game,
-			String date,
-			String award,
-			String description) {
-		this.name = name;
-		this.game = game;
-		this.date = date;
-		this.award = award;
-		this.description = description;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return this.name;
-	}
-	
-	
-	public void setGame(String game) {
-		this.game = game;
-	}
-	public String getGame() {
-		return this.game;
-	}
-	
-	
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getDate() {
-		return this.date;
-	}
-	
-	
-	public String getHour() {
-		return hour;
-	}
-
-	public void setHour(String hour) {
-		this.hour = hour;
-	}
-
-	public void setAward(String award) {
-		this.award = award;
-	}
-	public String getAward() {
-		return this.award;
-	}
-	
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getDescription() {
-		return this.description;
-	}
 	
 
 }
