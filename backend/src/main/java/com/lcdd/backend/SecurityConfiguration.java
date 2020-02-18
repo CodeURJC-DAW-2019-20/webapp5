3f44cf02f2ac7edac105811ca16d96758a0f14c2
Daniel <Danielmg_21@hotmail.es>
2020-02-18 13:07:47 +0100

    Starting user roles

--- backend/src/main/java/com/lcdd/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/lcdd/backend/SecurityConfiguration.java
@@ -1,21 +1,55 @@
 package com.lcdd.backend;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	
+	@Override 
+	protected void configure(HttpSecurity http)throws Exception{
 		// Public pages
+		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/about_us").permitAll();
+		http.authorizeRequests().antMatchers("/contact").permitAll();
+		http.authorizeRequests().antMatchers("/events").permitAll();
+		http.authorizeRequests().antMatchers("/merchandaidsing").permitAll();
+		http.authorizeRequests().antMatchers("/register").permitAll();
 		
-		// Private pages (all other pages)
+		// Private pages
+		http.authorizeRequests().antMatchers("/merch-form").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers("/event-form").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
 		
 		// Login form
+		http.formLogin().loginPage("/login");
+		http.formLogin().usernameParameter("username");
+		http.formLogin().passwordParameter("password");
+		http.formLogin().defaultSuccessUrl("/");
+		http.formLogin().failureUrl("/loginerror");
 		
-		// Logout
+		// Log out
+		http.logout().logoutUrl("/logout");
+		http.logout().logoutSuccessUrl("/");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+		// Users
+		auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("adminpass").roles("USER","ADMIN");
 		
 		// Disable CSRF at the moment
 		//http.csrf().disable();
 	}
+	
 }
+
+	
+	
+
+
+
 
