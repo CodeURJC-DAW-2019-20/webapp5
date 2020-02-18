df0aeb4ec5a36c993d13960c08bd35bc240c3b77
carlospabe <46899099+carlospabe@users.noreply.github.com>
2020-02-18 09:55:34 +0100

    removed useless comments

--- js/show_more_events.js
+++ js/show_more_events.js
@@ -1,9 +1,3 @@
-// $(document).ready(function () {
-//     $("#show_more").click(function () {
-//         $(".original").append("<p>Test</p>");
-//     });
-// });
-
 function showMoreEvents() {
     var new_row = $('<div class="row"><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div></div><br>').hide();
     $('#original').append(new_row);
