bed4ec856fa68f79542340751778df3550c7e3f8
carlospabe <46899099+carlospabe@users.noreply.github.com>
2020-02-18 09:54:41 +0100

    renamed variables and removed useless comments

--- js/show_more_events.js
+++ js/show_more_events.js
@@ -5,9 +5,9 @@
 // });
 
 function showMoreEvents() {
-    var new_item = $('<div class="row"><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div></div><br>').hide();
-    $('#original').append(new_item);
-    new_item.slideDown();
+    var new_row = $('<div class="row"><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div></div><br>').hide();
+    $('#original').append(new_row);
+    new_row.slideDown();
 }
 
     
