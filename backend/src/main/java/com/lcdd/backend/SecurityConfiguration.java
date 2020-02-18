f473ab57b44d5f0291596ec47559af67d51bef5b
Miguel Jimenez <migueljg1997@gmail.com>
2020-02-18 11:11:55 +0100

    DB clases, relations and db in my sql configured

--- backend/src/main/java/com/lcdd/backend/SecurityConfiguration.java
+++ backend/src/main/java/com/lcdd/backend/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package com.lcdd.backend;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Public pages
+		http.httpBasic().disable();
 		
 		// Private pages (all other pages)
 		
@@ -18,4 +21,3 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		//http.csrf().disable();
 	}
 }
-
