6f37ab68ce192c1e4e80cd60f2542e18ee52ee5e
carlospabe <46899099+carlospabe@users.noreply.github.com>
2020-02-24 11:10:33 +0100

    removed scroll to button functionality

--- js/show_more_events.js
+++ js/show_more_events.js
@@ -1,14 +1,5 @@
 function showMoreEvents() {
     var new_row = $('<div class="row"><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div></div><br>').hide();
     $('#original').append(new_row);
-    scrollToButton(new_row.slideDown(150));
-    
+    new_row.slideDown();
 }
-
-function scrollToButton() {
-    var elmnt = document.getElementById("show_more_button");
-    elmnt.scrollIntoView({
-        behavior: "smooth"
-    });
-}
-
