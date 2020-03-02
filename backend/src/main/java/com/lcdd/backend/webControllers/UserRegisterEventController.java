a81fde4dd82d810cd886291cc796a3bfb44915c2
oscar <oscar.rivas.99@gmail.com>
2020-03-02 23:27:04 +0100

    updates

--- backend/src/main/java/com/lcdd/backend/webControllers/UserRegisterEventController.java
+++ backend/src/main/java/com/lcdd/backend/webControllers/UserRegisterEventController.java
@@ -8,20 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lcdd.backend.UserSession;
+import com.lcdd.backend.dbrepositories.UserRepository;
+import com.lcdd.backend.pojo.Event;
+import com.lcdd.backend.pojo.User;
+import com.lcdd.backend.pojo.EventRegister;
 
 public class UserRegisterEventController {
 	
 	@Autowired
 	private UserSession session;
+	@Autowired
+	private UserRepository userRepository;
+	
 	
 	@RequestMapping("eventUserRegister")
-	public void serveEvent(Model model,@RequestParam int participants, HttpServletRequest request) {
+	public void serveEvent(Model model,@RequestParam String participants, HttpServletRequest request) {
 		
 		if (session.getIsLogggedIn()) {
 			model.addAttribute("logged",session.getIsLogggedIn());
 			model.addAttribute("session", session);
-	
-			//EventRegister register = new EventRegister(session.,event,user.getName(),participants);
+			User user = userRepository.findById(session.getId()).get();
+			
+			
+			//EventRegister register = new EventRegister(user,"lol",user.getName(),participants);
 			System.out.println("registrado correctamente");
 		}else {
 			System.out.println("no registrado");
