6e3238f1a774a0c8af9c153e793b4f498348312f
carlospabe <46899099+carlospabe@users.noreply.github.com>
2020-03-02 20:59:38 +0100

    events list pageable

--- backend/src/main/java/com/lcdd/backend/webControllers/EventsController.java
+++ backend/src/main/java/com/lcdd/backend/webControllers/EventsController.java
@@ -1,65 +1,111 @@
 package com.lcdd.backend.webControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.lcdd.backend.pojo.Event;
 import com.lcdd.backend.dbrepositories.EventRepository;
 
-import java.util.Iterator;
-import java.util.List;
+//import java.util.Iterator;
+//import java.util.List;
 
 @Controller
 public class EventsController {
 	
 	//private List<Event> eventsList;
 	@Autowired
-	private static EventRepository repository;
+	private EventRepository repository;
 	
-	private static int eventsShownCounter = 1;
-	private static Iterator<Event> eventsListIterator = getEventsList().iterator();
+//	private static int eventsShownCounter = 1;
+//	private Iterator<Event> eventsListIterator;
 	
+	
+	
+	
+
 	@GetMapping(value= {"events"})
 	public String serveEvents(Model model) {
 		return "events";
 	}
 	
-	private static List<Event> getEventsList() {
-		return repository.findAll();
-	}
-	
-	public String getCodeblockSixFirstEvents() {
-		String codeblock = "";
-		while ((eventsListIterator.hasNext()) && (eventsShownCounter <= 6)) {
-			Event event = eventsListIterator.next();
-			codeblock += getCodeblockNextColumn(event);
-			eventsShownCounter++;
+//	**************COSAS GRANIZO*****************
+	@GetMapping("/eventList")
+	public String getEventsList(@RequestParam() int pageId) {
+		String result = "";
+		
+		Page<Event> pageEvent = repository.findAll(PageRequest.of(pageId,6));
+		
+		for(Event event : pageEvent) {
+			if (event.isHaveImage()) {
+				result += "<div class='col-sm-4'>" + 
+								"<a href='/event/" + event.getId() + "'>" + 
+									"<img src='/images/eventsImages/image-" + event.getId() +".jpg' width='300'>" + 
+									"<h3 class='mt-2'>" + event.getName() + "</h3>" + 
+								"</a>" + 
+								"<p>" + event.getDescription() + "</p>" + 
+							"</div>";
+			} else {
+				result += "<div class='col-sm-4'>" + 
+								"<a href='#'>" + 
+									"<img src='/assets/img/events/placeholder_event_img.jpg' width='300'>" + 
+									"<h3 class='mt-2'>" + event.getName() + "</h3>" + 
+								"</a>" + 
+								"<p>" + event.getDescription() + "</p>" + 
+							"</div>";
+			}
+			
 		}
-		return codeblock;
+		System.out.println(pageId);
+		return result;
 	}
 	
-	public String getCodeblockNextThreeEvents() {
-		String codeblock = "";
-		int newEventsCounter = 1;
-		while ((eventsListIterator.hasNext()) && (newEventsCounter <= 3)) {
-			Event event = eventsListIterator.next();
-			codeblock += getCodeblockNextColumn(event);
-			newEventsCounter++;
-			eventsShownCounter++;
-		}
-		return codeblock;
-	}
+
 	
-	private String getCodeblockNextColumn(Event event) {
-		return "<div class=\"col-sm-4\">" + 
-					"<a href=\"#\">" + 
-						"<img src=\"./assets/img/events/event.jpg\" width=\"300\">" + 
-						"<h3 class=\"mt-2\">" + event.getName() + "</h3>" + 
-					"</a>" + 
-					"<p>" + event.getDescription() + "</p>" + 
-				"</div>";
-	}
+	
+	
+	
+//	*************COSAS CARLOS***********
+//	private List<Event> getEventsList() {
+//		return repository.findAll();
+//	}
+//	
+//	public String getCodeblockSixFirstEvents() {
+//		this.eventsListIterator = getEventsList().iterator();
+//		String codeblock = "";
+//		while ((eventsListIterator.hasNext()) && (eventsShownCounter <= 6)) {
+//			Event event = eventsListIterator.next();
+//			codeblock += getCodeblockNextColumn(event);
+//			eventsShownCounter++;
+//		}
+//		return codeblock;
+//	}
+//	
+//	public String getCodeblockNextThreeEvents() {
+//		String codeblock = "";
+//		int newEventsCounter = 1;
+//		while ((eventsListIterator.hasNext()) && (newEventsCounter <= 3)) {
+//			Event event = eventsListIterator.next();
+//			codeblock += getCodeblockNextColumn(event);
+//			newEventsCounter++;
+//			eventsShownCounter++;
+//		}
+//		return codeblock;
+//	}
+//	
+//	private String getCodeblockNextColumn(Event event) {
+//		return "<div class=\"col-sm-4\">" + 
+//					"<a href=\"#\">" + 
+//						"<img src=\"./assets/img/events/event.jpg\" width=\"300\">" + 
+//						"<h3 class=\"mt-2\">" + event.getName() + "</h3>" + 
+//					"</a>" + 
+//					"<p>" + event.getDescription() + "</p>" + 
+//				"</div>";
+//	}
 
 
 }
--- backend/src/main/resources/static/assets/js/show_more_events.js
+++ backend/src/main/resources/static/assets/js/show_more_events.js
@@ -1,10 +1,32 @@
-$(document).ready(function() {
-    var columns = $({{getCodeblockSixFirstEvents()}});
-    $(".main_row").append(columns);
-});
+//$(document).ready(function() {
+//    var columns = $('{{getCodeblockSixFirstEvents()}}');
+//    $("#main_row").append(columns);
+//});
+//
+//
+//function showMoreEvents() {
+//	var columns = $('{{getCodeblockNextThreeEvents()}}');
+//	$("#main_row").append(columns);
+//	columns.slideDown();
+//}
 
-function showMoreEvents() {
-	var columns = $({{getCodeblockNextThreeEvents()}});
-	$(".main_row").append(columns);
-	columns.slideDown();
-}
+
+$(document).ready(function() {
+	
+	$("#show_more_button").click(function(){
+    	var nextPageId = $(this).attr("nextPage");
+    	
+    	$.ajax({
+            url: '/eventList?pageId=' + nextPageId,
+        }).done(function (items) {
+//            var result = JSON.stringify(items);
+//          console.log(items);
+//          console.log(result);
+            
+            $("#main_row").append(items);
+            $("#main_row").append(nextPageId);
+            $("#show_more_button").attr("nextPage", Number(nextPageId) + 1);
+        })
+    	
+    });
+});
