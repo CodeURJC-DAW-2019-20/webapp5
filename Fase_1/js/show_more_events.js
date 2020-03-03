1c2c782123ca88ae8132d33d0e45c19021453570
Oscar Rivas <oscar.rivas.99@gmail.com>
2020-03-03 11:32:44 +0100

    general updates

--- Fase_1/js/show_more_events.js
+++
@@ -1,5 +0,0 @@
-function showMoreEvents() {
-    var new_row = $('<div class="row"><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div><div class="col-sm-4"><a href="#"><img src="./assets/img/events/event.jpg" width="300"><h3 class="mt-2">Evento extra</h3></a><p>Texto de ejemplo del evento extra</p></div></div><br>').hide();
-    $('#original').append(new_row);
-    new_row.slideDown();
-}
