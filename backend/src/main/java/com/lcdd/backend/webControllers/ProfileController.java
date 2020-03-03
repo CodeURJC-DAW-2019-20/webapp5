01e662d89e053f94bdcd46f8f0c897591b37902b
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2020-03-03 09:33:10 +0100

    Advanced algorim

--- backend/src/main/java/com/lcdd/backend/webControllers/ProfileController.java
+++ backend/src/main/java/com/lcdd/backend/webControllers/ProfileController.java
@@ -1,5 +1,8 @@
 package com.lcdd.backend.webControllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lcdd.backend.UserSession;
+import com.lcdd.backend.dbrepositories.EventRepository;
+import com.lcdd.backend.dbrepositories.GameRepository;
 import com.lcdd.backend.dbrepositories.UserRepository;
+import com.lcdd.backend.pojo.Event;
+import com.lcdd.backend.pojo.EventRegister;
+import com.lcdd.backend.pojo.Game;
 import com.lcdd.backend.pojo.RegisterDataForm;
 import com.lcdd.backend.pojo.User;
 
@@ -19,12 +27,43 @@ public class ProfileController {
 	@Autowired
 	UserRepository userRepository;
 	@Autowired
+	EventRepository eventRepository;
+	@Autowired
+	GameRepository gameRepository;
+	@Autowired
 	private UserSession session;
 	
 	@GetMapping("profile")
 	public String profileModel(Model model, HttpServletRequest request) {
 		model.addAttribute("session", session);
-		System.out.println(session.getFirstName());
+		
+		Iterable<Event> eventList = eventRepository.findAll();
+		int count[] = new int[5];
+		
+		for(Event event: eventList) {
+			Game game = event.getGame();
+				long gameId = game.getId();
+					count[(int)gameId]++;
+			
+		}
+		
+		int posicionmayor = 0;
+        /* for(int i=0; i<count.length && i<count.length; i++){
+            if(count[i]>numeromayor){
+                numeromayor = count[i];
+            }
+        } */
+	        
+        for ( int i = 1; i < count.length; i++ )
+        {
+        	if (count[i] > count[posicionmayor] ) posicionmayor = i;
+        }
+	       
+		//long p = 1;  
+		Optional<Game> popular = gameRepository.findById((long)count[posicionmayor]);
+		//Optional<Game> popular = gameRepository.findById(p);   
+		model.addAttribute("popular", popular.get());
+		
 		return "profile";
 	}
 	
@@ -41,8 +80,7 @@ public class ProfileController {
 		session.setEmail(user.getEmail());
 		session.setFirstName(user.getFirstName());
 		session.setLastName(user.getLastName());
-		
-		
+				
 		userRepository.save(edit);
 		return "profile";
 	}
