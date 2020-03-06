913aba3c664355747a301ee05c506c096f3eff0b
Miguel Jimenez <migueljg1997@gmail.com>
2020-03-06 01:01:43 +0100

    build script ready

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -8,16 +8,17 @@
 path_docker=$(pwd)
 path_project=$(dirname $(pwd))/backend
 path_jar=$path_project/target
-name_image=$1
+#name_image=$1
 
 echo "Compiling java application"
 cd $path_project
+#docker run -it --rm -v "$(pwd)":/usr/src/backend -w /usr/src/backend maven:3.6.1-jdk-8-alpine /bin/bash
 docker run -it --rm -v "$(pwd)":/usr/src/backend -w /usr/src/backend maven:3.6.1-jdk-8-alpine mvn package -DskipTests
 
-#echo "Copying java application"
-#cd $path_docker
-#mv $path_jar/SpringDocker.jar .
-#mv SpringDocker.jar webapp-3.java
+echo "Copying java application"
+cd $path_docker
+mv $path_jar/backend-0.0.1-SNAPSHOT.jar .
+mv backend-0.0.1-SNAPSHOT.jar backend.java
 
 #echo "Creating docker image"
 #docker build -t codeurjc/$name_image .
