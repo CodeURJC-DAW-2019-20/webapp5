244c51295be97054af68e72dcaf9b7b94cebca70
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2020-03-08 13:05:56 +0100

    Fixed controllers

--- backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
+++ backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
@@ -1,165 +1,77 @@
 package com.lcdd.backend.restControllers;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lcdd.backend.pojo.Merchandising;
 import com.lcdd.backend.services.MerchandisingService;
 
+
 @RestController
+@RequestMapping("/api/merchandising")
 public class MerchandisingRestController {
 	
 	@Autowired
 	private MerchandisingService service;
 
-	
-	@RequestMapping(value= {"merchandising"})
-	public String serveMerchandising(Model model) {
-		return "merchandising";
+	@GetMapping("/")
+	public Collection<Merchandising> getMerchandising() {
+		return service.findAll();
 	}
-	
-	@RequestMapping("/merchandising/{id}")
-    public String getMerchandising(@PathVariable long id, Model model) {
-        Optional<Merchandising> merchFound = service.findById(id);
 
-        if(!merchFound.isPresent()) {
-        	return "redirect:/error";
-        }
-
-        model.addAttribute("merch", merchFound.get());
-
-        return "merchandising-template";
-    }
-	
-	@RequestMapping(value = {"/purchaseMerch/{id}"}, 
-			method = RequestMethod.POST)
-	public String purchaseMerch(Model model, @PathVariable long id) {
-		
-		Merchandising merchFound = service.findById(id).get();
-		
-        if(merchFound == null) {
-        	return "redirect:/error";
-        }
-        
-        merchFound.setStock(merchFound.getStock()-1);
-        service.save(merchFound);
-        model.addAttribute("merch", merchFound);
-        
-        return "merchandising-template";
+	@GetMapping("/titles")
+	public Collection<String> getMerchNames() {
+		return service.findAll().stream().map(b -> b.getName()).collect(Collectors.toList());
 	}
-	
-	
-	
-	@GetMapping("/merchList")
-	public ResponseEntity<String> getEventsList(@RequestParam() int pageId) {
-		String result = "";
-		
-		Page<Merchandising> pageMerch = service.findAllPages(pageId,3);
-		
-		if(pageMerch.hasContent()) {
-			for(Merchandising merch : pageMerch.getContent()) {
-				if(merch.getDiscount() > 0) {
-					if (merch.isHaveImage()) {
-						result += "<div class='col-lg-4 col-sm-6'>" +
-							"<div class='l_product_item'>" +
-								"<h4>" + merch.getName() + "</h4>" +
-								"<h5>" + merch.getPrice() + "€-" + merch.getDiscount() + "%</h5>" +
-								"<div class='l_p_img'>" +
-									"<img src='/images/merchImages/image-" + merch.getId() +".jpg' width='300' height='300'>" +
-									"<h5 class='sale'>Opciones:</h5>" +
-								"</div>" +
-								"<div class='l_p_text'>" +
-									"<ul>" +
-										"<li class='p_icon'><a href='/merchandising/" + merch.getId() + "'>Ver Detalles</a></li>" +
-									"</ul>" +
-								"</div>" +
-							"</div>" +
-						"</div>";
-					} else {
-						result += "<div class='col-lg-4 col-sm-6'>" +
-							"<div class='l_product_item'>" +
-								"<h4>" + merch.getName() + "</h4>" +
-								"<h5>" + merch.getPrice() + "€-" + merch.getDiscount() + "%</h5>" +
-								"<div class='l_p_img'>" +
-									"<img src='/assets/img/merch-image.jpg' width='300' height='300'>" +
-									"<h5 class='sale'>Opciones:</h5>" +
-								"</div>" +
-								"<div class='l_p_text'>" +
-									"<ul>" +
-										"<li class='p_icon'><a href='/merchandising/" + merch.getId() + "'>Ver Detalles</a></li>" +
-									"</ul>" +
-								"</div>" +
-							"</div>" +
-						"</div>";
-					}
-				} else {
-					if (merch.isHaveImage()) {
-						result += "<div class='col-lg-4 col-sm-6'>" +
-							"<div class='l_product_item'>" +
-								"<h4>" + merch.getName() + "</h4>" +
-								"<h5>" + merch.getPrice() + "€</h5>" +
-								"<div class='l_p_img'>" +
-								"<img src='/images/merchImages/image-" + merch.getId() +".jpg' width='300' height='300'>" +
-									"<h5 class='sale'>Opciones:</h5>" +
-								"</div>" +
-								"<div class='l_p_text'>" +
-									"<ul>" +
-										"<li class='p_icon'><a href='/merchandising/" + merch.getId() + "'>Ver Detalles</a></li>" +
-									"</ul>" +
-								"</div>" +
-							"</div>" +
-						"</div>";
-					} else {
-						result += "<div class='col-lg-4 col-sm-6'>" +
-							"<div class='l_product_item'>" +
-								"<h4>" + merch.getName() + "</h4>" +
-								"<h5>" + merch.getPrice() + "€" + "</h5>" +
-								"<div class='l_p_img'>" +
-									"<img src='/assets/img/merch-image.jpg' width='300' height='300'>" +
-									"<h5 class='sale'>" + "Opciones:" + "</h5>" +
-								"</div>" +
-								"<div class='l_p_text'>" +
-									"<ul>" +
-										"<li class='p_icon'><a href='/merchandising/" + merch.getId() + "'>Ver Detalles</a></li>" +
-									"</ul>" +
-								"</div>" +
-							"</div>" +
-						"</div>";
-					}
-				}
-				
-//				result += "<div class='col-lg-4 col-sm-6'>" +
-//						"<div class='l_product_item'>" +
-//							"<h4>" + merch.getName() + "</h4>" +
-//							"<h5>" + merch.getPrice() + "€" + "</h5>" +
-//							"<div class='l_p_img'>" +
-//								"<img src='" + "/assets/img/products/lcdd_mug.png" + "' width='300' height='300'>" +
-//								"<h5 class='sale'>" + "Opciones:" + "</h5>" +
-//							"</div>" +
-//							"<div class='l_p_text'>" +
-//								"<ul>" +
-//									"<li class='p_icon'><a href='" + "#" +"'>Ver Detalles</a></li>" +
-//								"</ul>" +
-//							"</div>" +
-//						"</div>" +
-//					"</div>";
-			}
-		} else {
-			result += "nomore";
-		}
-		
-		return new ResponseEntity<>(result, HttpStatus.OK);
+
+	@GetMapping("/{id}")
+	public Merchandising getMerchandising(@PathVariable long id) {
+		return service.findById(id).get();
 	}
+
+	@PostMapping("/")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Merchandising createBook(@RequestBody Merchandising merch) {
+		service.save(merch);
+		System.out.println(merch.getId());
+		return merch;
+	}
+
+	@PutMapping("/{id}")
+	public Merchandising updateBook(@PathVariable long id, @RequestBody Merchandising updatedMerch){
+
+		service.findById(id).get(); //Returns with 404 if not found in database
+		
+		updatedMerch.setId(id);
+		service.save(updatedMerch);
+		return updatedMerch;
+	}
+
+	@DeleteMapping("/{id}")
+	public Merchandising deleteBook(@PathVariable long id) {
+
+		Merchandising deletedBook = service.findById(id).get();
+		service.delete(id);
+		return deletedBook;
+	}
+
 
 }
