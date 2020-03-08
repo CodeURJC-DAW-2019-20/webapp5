a92083a940881cba75ded0abac5978aad7eb9dd9
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2020-03-08 13:19:48 +0100

    Update MerchandisingRestController.java

--- backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
+++ backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
@@ -1,14 +1,10 @@
 package com.lcdd.backend.restControllers;
 
 import java.util.Collection;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
