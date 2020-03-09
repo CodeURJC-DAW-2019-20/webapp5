e2e7cfc819beb9ef5ada8a0e57cf4325597d2d85
Miguel Jimenez <migueljg1997@gmail.com>
2020-03-09 13:10:37 +0100

    everything working. lasting little changes before merging

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -10,8 +10,6 @@ path_project=$(dirname $(pwd))/backend
 path_jar=$path_project/target
 #name_image=$1
 
-
-
 echo "Compiling java application"
 cd $path_project
 #docker run -it --rm -v "$(pwd)":/usr/src/backend -w /usr/src/backend maven:3.6.1-jdk-8-alpine /bin/bash
@@ -20,9 +18,15 @@ docker run -it --rm -v "$(pwd)":/usr/src/backend -v "$HOME/.m2":/root/.m2 -w /us
 echo "Copying java application"
 cd $path_docker
 mv $path_jar/backend-0.0.1-SNAPSHOT.jar .
+rm backend.jar
 mv backend-0.0.1-SNAPSHOT.jar backend.jar
 
 echo "Creating docker image"
-#docker build -t bygranizo/lcdd_backend .
-#docker run -t lcdd_backend
+docker build -t bygranizo/lcdd_backend .
 docker push bygranizo/lcdd_backend
+
+
+#docker run -it --rm bygranizo/lcdd_backend /bin/bash
+
+#docker rmi --force bygranizo/lcdd_backend
+#docker build -t bygranizo/lcdd_backend .
