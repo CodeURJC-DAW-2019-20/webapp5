c717e918137292bb35c703033d93614cbf8792a6
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2020-03-09 11:43:29 +0100

    Update MerchandisingRestController.java

--- backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
+++ backend/src/main/java/com/lcdd/backend/restControllers/MerchandisingRestController.java
@@ -1,8 +1,6 @@
 package com.lcdd.backend.restControllers;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,7 +8,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lcdd.backend.pojo.MerchType;
