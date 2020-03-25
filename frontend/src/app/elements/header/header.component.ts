95c47d4ea082c01b97542221f06059db56ea41ca
Miguel Jimenez <migueljg1997@gmail.com>
2020-03-25 18:24:22 +0100

    head footer ready + assets

--- frontend/src/app/elements/header/header.component.ts
+++ frontend/src/app/elements/header/header.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public isMenuCollapsed = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
