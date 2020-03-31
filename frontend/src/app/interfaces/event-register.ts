1b3ad08b8d1ebfd4b0b61c361f4e7e606eb589a9
Miguel Jimenez <migueljg1997@gmail.com>
2020-03-31 18:47:48 +0200

    interfaces from pojo

--- frontend/src/app/interfaces/event-register.ts
+++ frontend/src/app/interfaces/event-register.ts
@@ -1,2 +1,10 @@
+import { Users } from './users';
+import { Events } from './events';
+
 export interface EventRegister {
+	id: number;
+	user: Users;
+	event: Events;
+	inscriptionName: string;
+	participantNumber: number;
 }
--- frontend/src/app/interfaces/roles.ts
+++ frontend/src/app/interfaces/roles.ts
@@ -1,2 +1,7 @@
+import { Users } from './users';
+
 export interface Roles {
+	id: number;
+	name: string;
+	users?: Users[];
 }
