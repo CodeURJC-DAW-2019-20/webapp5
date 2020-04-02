6fff27eb854b3f542362e879df75ea35ba2b2950
Miguel Jimenez <migueljg1997@gmail.com>
2020-04-02 19:48:54 +0200

    some changes in dockerizing, not working yet

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -9,14 +9,15 @@ path_project_back=$(dirname $(pwd))/backend
 path_jar=$path_project_back/target
 
 mkdir -p $path_project_back/src/main/resources/public/new
-path_front_location_in_back=$path_project_back/src/main/public/new
+path_front_location_in_back=$path_project_back/src/main/resources/public/new
 
 echo "Building frontend"
 cd $path_project_front
 
-docker run --rm -it -v $(pwd):/frontend -w /frontend teracy/angular-cli:8.3 npm install --no-bin-links && ng build
+docker run --rm -it -v $(pwd):/frontend -w /frontend teracy/angular-cli:8.3 npm install --no-bin-links && ng build --prod --base-href="https://localhost:8443/new/"
 
 echo "Moving frontend"
+rm -r $path_front_location_in_back/
 cp -a $path_dist/. $path_front_location_in_back
 #echo "Compiling java application"
 #cd $path_project_back
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndPoint: 'https://localhost:8443/api',
 };
