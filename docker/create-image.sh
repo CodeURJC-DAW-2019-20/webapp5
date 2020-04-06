799eb51ae16722a1e5ba3e31183a061d0db954c6
Miguel Jimenez <migueljg1997@gmail.com>
2020-04-06 17:38:59 +0200

    little fix in dockerize

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -14,7 +14,8 @@ path_front_location_in_back=$path_project_back/src/main/resources/static/new
 echo "Building frontend"
 cd $path_project_front
 
-docker run --rm -it -v $(pwd):/frontend -w /frontend teracy/angular-cli:8.3 npm install --no-bin-links && ng build --prod --base-href="https://localhost:8443/new/"
+docker run --rm -it -v $(pwd):/frontend -w /frontend teracy/angular-cli:8.3 npm install --no-bin-links && ng build --prod --base-href=/new/
+#ng build --prod --base-href=/new/
 
 echo "Moving frontend"
 rm -r $path_front_location_in_back/
@@ -27,7 +28,6 @@ cp -a $path_dist/. $path_front_location_in_back
 #mv $path_jar/backend-0.0.1-SNAPSHOT.jar .
 #rm backend.jar
 #mv backend-0.0.1-SNAPSHOT.jar backend.jar
-#
 #
 #echo "Creating docker image"
 #docker build -t bygranizo/lcdd_backend .
