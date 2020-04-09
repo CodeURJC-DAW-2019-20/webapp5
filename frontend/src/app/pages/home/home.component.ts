6e208fcbf3e87980a43dfdce42487dfc8b4bddba
Miguel Jimenez <migueljg1997@gmail.com>
2020-04-09 22:40:52 +0200

    home fixed

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,23 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  styles: [`
-  /deep/ .carousel-control-prev-icon{
-    background-image: url("assets/img/home/prevFOTO.png") !important;
-    width: 50px !important;
-    height: 50px !important;
-  }
-  /deep/ .carousel-control-next-icon{
-    background-image: url("assets/img/home/nextFOTO.png") !important;
-    width: 50px !important;
-    height: 50px !important;
-  }
-  /deep/ .carousel-control-next, /deep/ .carousel-control-prev{
-    opacity: 1 !important;
-    z-index: 100001 !important;
-  }
-`]
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
