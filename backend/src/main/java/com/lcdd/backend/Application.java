70fd50f59a2fe769cbdc4e5113227fbc41ddaf06
Miguel Jimenez <migueljg1997@gmail.com>
2020-04-14 16:25:53 +0200

    /new/ redirect is working

--- backend/src/main/java/com/lcdd/backend/Application.java
+++ backend/src/main/java/com/lcdd/backend/Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -25,4 +26,10 @@ public class Application {
 			}
 		};
 	}
+
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/new").setViewName("redirect:/new/");
+			registry.addViewController("/new/").setViewName("forward:/new/index.html");
+    }
 }
