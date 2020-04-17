f71340b0002d220781184f052ba01d7abc3b1467
oscar <oscar.rivas.99@gmail.com>
2020-04-17 22:38:55 +0200

    create user registration in an event

--- backend/src/main/java/com/lcdd/backend/restControllers/EventRegisterRestController.java
+++ backend/src/main/java/com/lcdd/backend/restControllers/EventRegisterRestController.java
@@ -2,25 +2,57 @@ package com.lcdd.backend.restControllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lcdd.backend.UserSession;
+import com.lcdd.backend.pojo.Event;
 import com.lcdd.backend.pojo.EventRegister;
+import com.lcdd.backend.pojo.User;
+import com.lcdd.backend.services.EventService;
 import com.lcdd.backend.services.UserRegisterEventService;
+import com.lcdd.backend.services.UserService;
 
 @RestController
-@RequestMapping("/api/event-register")
+@RequestMapping("/api/userRegisterEvent")
 public class EventRegisterRestController {
 	@Autowired
 	private UserRegisterEventService eventRegisterService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private EventService eventService;
 	
+	//get all inscriptions
+	//only admin
 	@GetMapping(value={"", "/"})
 	public ResponseEntity<List<EventRegister>> getEventList() {
 		List<EventRegister> eventRegisterList = eventRegisterService.findAll();
 		return new ResponseEntity<>(eventRegisterList, HttpStatus.OK);
+	}
+	
+	//register user in an event
+	//need user registered
+	@PostMapping("/{id}")
+	public ResponseEntity<EventRegister> serveEvent(@PathVariable Long id, 
+			@RequestParam int participants, UserSession session, HttpServletRequest request) {
+		User user = userService.findByName(session.getUsername());
+		Event event = eventService.findById(id);
+		EventRegister register = new EventRegister(user,event,user.getName(),participants);
+		event.setMaxParticipants(event.getMaxParticipants()-participants);
+		eventService.save(event);
+		eventRegisterService.save(register);
+		return new ResponseEntity<>(register, HttpStatus.OK);
 	}
 }
--- frontend/src/app/pages/events/event-template/event-template.component.ts
+++ frontend/src/app/pages/events/event-template/event-template.component.ts
@@ -3,6 +3,9 @@ import { Events } from 'src/app/interfaces/events';
 import { EventsService } from 'src/app/services/events/events.service';
 import { throwError, Observable } from 'rxjs';
 import { faClock, faMapMarkerAlt, faGamepad, faChild, faUsers, faMoneyBillWave, faBoxes } from '@fortawesome/free-solid-svg-icons';
+import { LoginService } from 'src/app/services/login/login.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-template',
@@ -15,6 +18,10 @@ export class EventTemplateComponent implements OnInit {
   imageToShow: any;
   isImageLoading: boolean;
   eventNumber: number;
+  isLogged: boolean;
+  showRegisterEvent: boolean;
+  public inscription;
+
   //icon var
   faClock = faClock;
   faMapMarkerAlt = faMapMarkerAlt;
@@ -24,11 +31,24 @@ export class EventTemplateComponent implements OnInit {
   faMoneyBillWave = faMoneyBillWave;
   faBoxes = faBoxes;
 
-  constructor(protected eventsService: EventsService) {    
+  constructor(
+    protected eventsService: EventsService,
+    public loginService: LoginService,
+    public router: Router
+    ) { 
+      this.inscription = new FormGroup({
+        eventsReg: new FormControl(null,Validators.required)
+      });
    }
 
   // on init, get the event data
   ngOnInit(): void {
+    this.showRegisterEvent = false;
+    //this.isLogged = this.loginService.isLogged;
+    this.isLogged = true;
+    if(this.isLogged){
+      //this.router.navigate(['/error']);
+    }
     this.refreshEvent();
   }
 
@@ -57,8 +77,6 @@ export class EventTemplateComponent implements OnInit {
 
   //get the event image
   private getImageFromService() {
-    console.error(this.event.id);
-    
     this.eventsService.getImage(this.event.id).subscribe(data => {
       this.createImageFromBlob(data);
       this.isImageLoading = true;
@@ -67,8 +85,25 @@ export class EventTemplateComponent implements OnInit {
       console.log(error);
     });
   }
+
   private handleError(error: any) {
     console.error(error);
     return Observable.throw('Server error (' + error.status + '): ' + error);
+  }
+
+  showRegister(){
+    this.showRegisterEvent = true;
+  }
+
+  submit(){
+    this.showRegisterEvent = false;
+    console.log(this.showRegisterEvent);
+    console.error(this.inscription.value.eventsReg);
+    this.eventsService.saveInscription(this.event.id,this.inscription.value).subscribe(
+      response => {
+        console.log("register great");
+      },
+      error => this.handleError(error)
+    );
   }
 }
