44e6e087b883fa5ef6f1cae010321e5216464514
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2020-04-19 23:21:28 +0200

    Finalizado algoritmo avanzado

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorage } from 'ngx-webstorage';
 
+import { ProfileService } from 'src/app/services/profile/profile.service';
+
+import { Events } from 'src/app/interfaces/events';
+
+import { EventsService } from 'src/app/services/events/events.service';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,11 +17,73 @@ export class ProfileComponent implements OnInit {
 
   @LocalStorage('currentUser')
   public currentUser;
+  eventList;
+  gameId: number;
+  gameName: string;
+  aux: number;
   activeId;
+  active;
 
-  constructor() { }
+  constructor(protected profileService: ProfileService, protected eventsService: EventsService) { }
 
   ngOnInit(): void {
+    this.getUserEvents();
+  }
+
+  getUserEvents() {
+    this.profileService.getEventsRegistered(this.currentUser.id).subscribe(
+      data => {
+        this.eventList = data;
+        console.log(data);
+        this.getPopular();
+      },
+      error => this.handleError(error)
+      )
+    ;}
+
+  getPopular(){
+    console.log(this.eventList);
+    this.eventList.forEach(element => {
+      this.aux = element.event.game.id;
+      console.log(element.event.game.id);
+    })
+    console.log(this.aux);
+    switch (this.aux) {
+      case 1:
+        this.gameId = 1;
+        this.gameName = "League of Legends"
+        break;
+       
+      case 2:
+        this.gameId = 2;
+        this.gameName = "Magic: The Gathering"
+        break;
+
+      case 3:
+        this.gameId = 3;
+        this.gameName = "Teamfight Tactics"
+        break;      
+        
+      case 4:
+        this.gameId = 4;
+        this.gameName = "Legends of Runeterra"
+        break;
+
+      case 5:
+        this.gameId = 5;
+        this.gameName = "Hearthstone"
+        break;
+
+      default:
+        break;
+    }
+    console.log(this.gameId);
+    console.log(this.gameName);
+  }
+
+    
+  private handleError(error: any) {
+		console.error(error);
   }
 
 }
