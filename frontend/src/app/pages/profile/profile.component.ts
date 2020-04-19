311493a8d2a7c6e5a0e9fbd7dcb0f2f56c6850dd
carlospabe <46899099+carlospabe@users.noreply.github.com>
2020-04-19 18:01:17 +0200

    first steps on profile front

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorage } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  @LocalStorage('currentUser')
+  public currentUser;
 
   constructor() { }
 
